f785b58e266a79a08e378caa6641c062bbdc4099
eduardo-script <eduardovinicius.code@gmail.com>
2026-04-17 17:22:56 -0300

    condicionais exercicio

---
+++ condicionais.ts
@@ -0,0 +1,15 @@
+const nota1 = 8.5 
+const nota2 = 5.5
+const nota3 = 4.0
+
+const média = (nota1 + nota2 + nota3) 3
+
+//aluno aprovado so com média maior ou igual à 7.0
+
+
+if (média >= 7) {
+	console.log("Aluno aprovado com média: "+média)
+} else {
+	console.log("Aluno reprovado com média: "+média)
+}
+
