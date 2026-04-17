ba6632d38f7ea626b1ea745f898fc3eaed468511
eduardo-script <eduardovinicius.code@gmail.com>
2026-04-17 17:06:21 -0300

    aulas de operadores e aritmeticos

---
+++ mudar_valores.ts
@@ -0,0 +1,21 @@
+let a = 5
+let b = 10
+let c 
+
+const resultado = |(a > b)
+
+&& (and) e || (or)
+
+
+
+c = a //backup do valor de a
+a = b
+b = c
+
+
+// > < >= <- --- |-- operadores relacionais
+//
+//
+
+console.log('Valor de a: '+a)
+console.log('Valor de b: '+b)
--- operadores_aritmeticos.ts
+++ operadores_aritmeticos.ts
@@ -2,11 +2,11 @@ const numero1:number = 5
 const numero2:number = 2
 const numero3:number = 6
 
-//operadores aritimeticos
-const somar = numero1 + numero2
+// operadores aritimeticos
+const soma = numero1 + numero2
 const subtrair = numero1 - numero2
 const multiplicar = numero1 * numero2
-const dividir = mumero1 / numero2
+const dividir = numero1 / numero2
 const operação = numero3 * (numero1 + numero2)
 
 console.log("Soma: "+soma)
