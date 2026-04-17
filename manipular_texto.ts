c01d71edd80258eb264b29ae6032376fa2968e1a
eduardo-script <eduardovinicius.code@gmail.com>
2026-04-17 14:55:52 -0300

    Manipular texto

---
+++ manipular_texto.ts
@@ -0,0 +1,11 @@
+const texto = "O rato roeu a roupa do rei de roma"
+
+// Imprimir tamanho do texto
+console.log("Tamanho texto: "+texto.length)
+console.log("Texto em caixa alta: "+texto.toUpperCase())
+console.log("Texto em caixa baixa: "+texto.toLowerCase())
+console.log("Tem a palavra rato: "+texto.includes("rato"))
+console.log("Tem a palavra princisa: "+texto.includes("princesa"))
+console.log(texto.replace("rato","jose"))
+console.log(texto.replaceAll("a","@"))
+
--- variaveis.ts
+++ variaveis.ts
@@ -1,8 +1,13 @@
+const tela = require('readline-sync')
+// Declarando as váriaveis
 let nome:string
 let idade:number
 
-nome = "João"
-idade = 20
+// setando os valores
+nome = tela.question('Qual é o seu nome? ')
+idade = Number(tela.question('Qual é a sua idade? '))
 
+// saida das informações
 console.log("Nome: "+nome+" idade: "+idade)
+
 
