e641782c8141bbd0e9351449db660a82ce32c853
eduardo-script <eduardovinicius.code@gmail.com>
2026-04-17 16:05:55 -0300

    op aritmeticos

--- operadores_aritmeticos.ts
+++ operadores_aritmeticos.ts
@@ -1,6 +1,15 @@
-let ano:number = 2026
-// ano  = ano + 1
-ano += 1
-// ano = ano - 1
-ano -= 1
-console.log(ano)
+const numero1:number = 5
+const numero2:number = 2
+const numero3:number = 6
+
+//operadores aritimeticos
+const somar = numero1 + numero2
+const subtrair = numero1 - numero2
+const multiplicar = numero1 * numero2
+const dividir = mumero1 / numero2
+const operação = numero3 * (numero1 + numero2)
+
+console.log("Soma: "+soma)
+console.log("Subtração: "+ subtrair)
+console.log("Dividir: "+ dividir)
+console.log("Operação: "+ operação)
